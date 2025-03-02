171596924862a94ccd533f8429f27fe01f48b2a7
libsyfer <libsyfer@gmail.com>
2025-03-02 17:57:07 +0300

    Создан проект с программой умножения числа

---
+++ Lab2/Programs/Multiplication/Multiplication.cpp
@@ -0,0 +1,22 @@
+﻿// Multiplication.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+unsigned int mul(unsigned int a);
+
+int main()
+{
+    unsigned int num;
+    std::cout << "Enter number: ";
+    std::cin >> num;
+
+    int result = mul(num);
+
+    std::cout << "Mul: " << result << std::endl;
+}
+
+unsigned int mul(unsigned int a)
+{
+    return a * 4;
+};
